1edff3e5053a43983713cb4759f201ce96a36431
JedrzejChalubek <jedrzej.chalubek@gmail.com>
2014-06-22 00:15:36 +0200

    Normal

--- dist/app/app.js
+++ dist/app/app.js
@@ -475,19 +475,41 @@ var iosOverlay = function(params) {
 
 	$scope.showArticle = function (el) {
 
+		el.unread = '0';
+
 		angular.extend($scope.view, {
+			panel: false,
 			section: 'single',
 			single: el,
 			scrollPosition: $('#thumbs').scrollTop()
+		});
+
+		Api.UserArticle.update({
+			articleid: el.id
+		}, {
+			unread: el.unread,
+			favourite: el.favourite,
+			later: el.later
 		});
 
 	};
 
 	$scope.nextArticle = function (el) {
 
+		el.unread = '0';
+
 		angular.extend($scope.view, {
+			panel: false,
 			section: 'single',
 			single: el
+		});
+
+		Api.UserArticle.update({
+			articleid: el.id
+		}, {
+			unread: el.unread,
+			favourite: el.favourite,
+			later: el.later
 		});
 
 	};
@@ -582,19 +604,41 @@ var iosOverlay = function(params) {
 
 	$scope.showArticle = function (el) {
 
+		el.unread = '0';
+
 		angular.extend($scope.view, {
+			panel: false,
 			section: 'single',
 			single: el,
 			scrollPosition: $('#thumbs').scrollTop()
+		});
+
+		Api.UserArticle.update({
+			articleid: el.id
+		}, {
+			unread: el.unread,
+			favourite: el.favourite,
+			later: el.later
 		});
 
 	};
 
 	$scope.nextArticle = function (el) {
 
+		el.unread = '0';
+
 		angular.extend($scope.view, {
+			panel: false,
 			section: 'single',
 			single: el
+		});
+
+		Api.UserArticle.update({
+			articleid: el.id
+		}, {
+			unread: el.unread,
+			favourite: el.favourite,
+			later: el.later
 		});
 
 	};
--- src/app/controller/favourites.js
+++ src/app/controller/favourites.js
@@ -2,19 +2,41 @@ Reed.controller('Favourites', function ($scope, $filter, Api, State, Collection)
 
 	$scope.showArticle = function (el) {
 
+		el.unread = '0';
+
 		angular.extend($scope.view, {
+			panel: false,
 			section: 'single',
 			single: el,
 			scrollPosition: $('#thumbs').scrollTop()
+		});
+
+		Api.UserArticle.update({
+			articleid: el.id
+		}, {
+			unread: el.unread,
+			favourite: el.favourite,
+			later: el.later
 		});
 
 	};
 
 	$scope.nextArticle = function (el) {
 
+		el.unread = '0';
+
 		angular.extend($scope.view, {
+			panel: false,
 			section: 'single',
 			single: el
+		});
+
+		Api.UserArticle.update({
+			articleid: el.id
+		}, {
+			unread: el.unread,
+			favourite: el.favourite,
+			later: el.later
 		});
 
 	};
